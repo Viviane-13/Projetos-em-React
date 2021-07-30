39ee7935bc24176281b72a168d8b913a18f4cdeb
Viviane <oviviane772@gmail.com>
2021-07-30 14:19:02 -0300

    firebase

---
+++ sistema_de_chamados/src/services/firebaseConnection.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+
+import "firebase/auth";
+
+let firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_SENDER_ID,
+  appId: process.env.REACT_APP_PROJECT_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+if (!firebase.apps.length()) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
