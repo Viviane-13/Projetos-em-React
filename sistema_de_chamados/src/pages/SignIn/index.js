8c874971f3dea71da057a76db6d3e20c810a3581
Viviane <oviviane772@gmail.com>
2021-08-02 18:09:54 -0300

    page login

--- sistema_de_chamados/src/pages/SignIn/index.js
+++ sistema_de_chamados/src/pages/SignIn/index.js
@@ -1,7 +1,41 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./signin.css";
+import logo from "../../assets/logo.png";
+
 function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert("Clicouuu");
+  }
+
   return (
-    <div>
-      <h1>SignIn</h1>
+    <div className="container-center">
+      <div className="login">
+        <div className="login-area">
+          <img src={logo} alt="Sistema logo" />
+        </div>
+        <form onSubmit={handleSubmit}>
+          <h1>Entrar</h1>
+          <input
+            type="text"
+            placeholder="email@email.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="*******"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Acessar</button>
+        </form>
+        <Link to="/register">Criar uma conta</Link>
+      </div>
     </div>
   );
 }
