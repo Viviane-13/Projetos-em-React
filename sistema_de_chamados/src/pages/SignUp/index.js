e6f663900524706531da0d119918df7e923dbd18
Viviane <oviviane772@gmail.com>
2021-08-02 18:19:29 -0300

    page cadastro

--- sistema_de_chamados/src/pages/SignUp/index.js
+++ sistema_de_chamados/src/pages/SignUp/index.js
@@ -1,7 +1,48 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+import logo from "../../assets/logo.png";
+
 function SignUp() {
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert("Clicouuu");
+  }
+
   return (
-    <div>
-      <h1>Tela de Cadastro</h1>
+    <div className="container-center">
+      <div className="login">
+        <div className="login-area">
+          <img src={logo} alt="Sistema logo" />
+        </div>
+        <form onSubmit={handleSubmit}>
+          <h1>Cadastrar</h1>
+          <input
+            type="text"
+            placeholder="Seu nome"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="email@email.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="*******"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Cadastrar</button>
+        </form>
+        <Link to="/">Já tem uma conta? Entre</Link>
+      </div>
     </div>
   );
 }
