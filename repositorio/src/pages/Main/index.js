1ca9419ae644db92b3f42b0058514589eba66ebf
Viviane <oviviane772@gmail.com>
2021-08-29 13:20:36 -0300

    page Main

--- repositorio/src/pages/Main/index.js
+++ repositorio/src/pages/Main/index.js
@@ -1,11 +1,22 @@
 import React from "react";
-
-import { Title } from "./styles";
+import { FaGithub, FaPlus } from "react-icons/fa";
+import { Container, Form, SubmitButton } from "./styles";
 
 export default function Main() {
   return (
     <div>
-      <Title>Main</Title>
+      <Container>
+        <h1>
+          <FaGithub size={25} />
+          Meus Repositorios
+        </h1>
+        <Form onSubmit={() => {}}>
+          <input type="text" placeholder="Adicionar Repositorios" />
+          <SubmitButton>
+            <FaPlus color="#FFF" size={14} />
+          </SubmitButton>
+        </Form>
+      </Container>
     </div>
   );
 }
--- repositorio/src/pages/Main/styles.js
+++ repositorio/src/pages/Main/styles.js
@@ -1,5 +1,44 @@
 import styled from "styled-components";
 
-export const Title = styled.h1`
-  color: #ff0000;
+export const Container = styled.div`
+  max-width: 700px;
+  background: #fff;
+  border-radius: 4px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+  padding: 30px;
+  margin: 80px auto;
+
+  h1 {
+    font-size: 20px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    svg {
+      margin-right: 10px;
+    }
+  }
+`;
+export const Form = styled.form`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: row;
+
+  input {
+    flex: 1;
+    border: 1px solid #ddd;
+    padding: 10px 15px;
+    border-radius: 4px;
+    font-size: 17px;
+  }
+`;
+export const SubmitButton = styled.button`
+  background: #0d2636;
+  border: 0;
+  border-radius: 4px;
+  margin-left: 10px;
+  padding: 0 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
