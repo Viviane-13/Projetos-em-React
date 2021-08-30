a5dd68908c435c8716e80962bdb84dfd3762b4f2
Viviane <oviviane772@gmail.com>
2021-08-30 08:53:03 -0300

    list repos

--- repositorio/src/pages/Main/index.js
+++ repositorio/src/pages/Main/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useCallback } from "react";
-import { FaGithub, FaPlus, FaSpinner } from "react-icons/fa";
-import { Container, Form, SubmitButton } from "./styles";
+import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from "react-icons/fa";
+import { Container, Form, SubmitButton, List, DeleteButton } from "./styles";
 
 import api from "../../services/api";
 
@@ -36,6 +36,15 @@ export default function Main() {
   function handleInputChange(e) {
     setNewRepo(e.target.value);
   }
+
+  const handleDelete = useCallback(
+    (repo) => {
+      const find = repositorios.filter((r) => r.name !== repo);
+      setRepositorios(find);
+    },
+    [repositorios]
+  );
+
   return (
     <div>
       <Container>
@@ -58,6 +67,23 @@ export default function Main() {
             )}
           </SubmitButton>
         </Form>
+
+        <List>
+          {repositorios.map((repo) => (
+            <li key={repo.name}>
+              <span>
+                <DeleteButton onClick={() => handleDelete(repo.name)}>
+                  <FaTrash size={14} />
+                </DeleteButton>
+                {repo.name}
+              </span>
+
+              <a href="">
+                <FaBars size={20} />
+              </a>
+            </li>
+          ))}
+        </List>
       </Container>
     </div>
   );
--- repositorio/src/pages/Main/styles.js
+++ repositorio/src/pages/Main/styles.js
@@ -68,3 +68,34 @@ export const SubmitButton = styled.button.attrs((props) => ({
       }
     `}
 `;
+export const List = styled.ul`
+  list-style: none;
+  margin-top: 20px;
+
+  li {
+    padding: 15px 0;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+
+    & + li {
+      border-top: 1px solid #eee;
+    }
+    a {
+      color: #0d2636;
+      text-decoration: none;
+    }
+  }
+`;
+
+export const DeleteButton = styled.button.attrs({
+  type: "button",
+})`
+  background: transparent;
+  color: #0d2636;
+  border: 0;
+  padding: 8px 7px;
+  outline: 0;
+  border: 4px;
+`;
