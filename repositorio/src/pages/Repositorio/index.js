9f6dcbc0109cec7594df9d93a4c4f02fc5b13fd7
Viviane <oviviane772@gmail.com>
2021-08-31 09:58:19 -0300

    paginação

--- repositorio/src/pages/Repositorio/index.js
+++ repositorio/src/pages/Repositorio/index.js
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { Container, Owner, Loading, BackButton } from "./styles";
+import {
+  Container,
+  Owner,
+  Loading,
+  BackButton,
+  IssuesList,
+  PageActions,
+} from "./styles";
 
 import { FaArrowLeft } from "react-icons/fa";
 
@@ -9,6 +16,7 @@ export default function Repositorio({ match }) {
   const [repositorio, setRepositorio] = useState({});
   const [issues, setIssues] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     async function load() {
@@ -29,6 +37,26 @@ export default function Repositorio({ match }) {
     load();
   }, [match.params.repositorio]);
 
+  useEffect(() => {
+    async function loadIssue() {
+      const nomeRepo = decodeURIComponent(match.params.repositorio);
+
+      const response = await api.get(`/repos/${nomeRepo}/issues`, {
+        params: {
+          state: "open",
+          page,
+          per_page: 5,
+        },
+      });
+      setIssues(response.data);
+    }
+    loadIssue();
+  }, [match.params.repositorio, page]);
+
+  function handlePage(action) {
+    setPage(action === "back" ? page - 1 : page + 1);
+  }
+
   if (loading) {
     return (
       <Loading>
@@ -46,6 +74,34 @@ export default function Repositorio({ match }) {
         <h1>{repositorio.name}</h1>
         <p>{repositorio.description}</p>
       </Owner>
+      <IssuesList>
+        {issues.map((issue) => (
+          <li key={String(issue.id)}>
+            <img src={issue.user.avatar_url} alt={issue.user.login} />
+            <div>
+              <strong>
+                <a href={issue.html_url}>{issue.title}</a>
+                {issue.labels.map((label) => (
+                  <span key={String(label.id)}>{label.name}</span>
+                ))}
+              </strong>
+              <p>{issue.user.login}</p>
+            </div>
+          </li>
+        ))}
+      </IssuesList>
+      <PageActions>
+        <button
+          type="button"
+          onClick={() => handlePage("back")}
+          disabled={page < 2}
+        >
+          Voltar
+        </button>
+        <button type="button" onClick={() => handlePage("next")}>
+          Próxima
+        </button>
+      </PageActions>
     </Container>
   );
 }
--- repositorio/src/pages/Repositorio/styles.js
+++ repositorio/src/pages/Repositorio/styles.js
@@ -45,3 +45,79 @@ export const BackButton = styled(Link)`
   outline: 0;
   background: transparent;
 `;
+
+export const IssuesList = styled.ul`
+  margin-top: 30px;
+  padding-top: 30px;
+  border-top: 1px solid #eee;
+  list-style: none;
+
+  li {
+    display: flex;
+    padding: 15px 10px;
+
+    & + li {
+      margin-top: 12px;
+    }
+
+    img {
+      width: 36px;
+      height: 36px;
+      border-radius: 50%;
+      border: 2px solid #0d2636;
+    }
+
+    div {
+      flex: 1;
+      margin-left: 12px;
+
+      p {
+        margin-top: 10px;
+        font-size: 12px;
+        color: #000;
+      }
+
+      strong {
+        font-size: 15px;
+
+        a {
+          text-decoration: none;
+          color: #222;
+          transform: 0.3s;
+          &:hover {
+            color: #0071db;
+          }
+        }
+        span {
+          background: #222;
+          color: #fff;
+          border-radius: 4px;
+          font-size: 12px;
+          font-weight: 600;
+          padding: 5px 7px;
+          margin-left: 10px;
+        }
+      }
+    }
+  }
+`;
+
+export const PageActions = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  button {
+    outline: 0;
+    border: 0;
+    background: #222;
+    color: #fff;
+    padding: 5px 10px;
+    border-radius: 4px;
+
+    &:disabled {
+      cursor: not-allowed;
+      opacity: 0.5;
+    }
+  }
+`;
